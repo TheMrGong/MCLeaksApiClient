7c1b89a21ab3dada76c828cec994c3888c905077
TheMrGong <gongora656@gmail.com>
2017-04-17 15:37:52 -0400

    Updated to use subdomain again.
    Removed throws NullPointerException, key no longer in use.
    Added javadocs to built jar.

--- src/main/java/me/gong/mcleaks/MCLeaksAPI.java
+++ src/main/java/me/gong/mcleaks/MCLeaksAPI.java
@@ -67,7 +67,6 @@ public interface MCLeaksAPI {
          * Builds a new MCLeaks API
          *
          * @return The built API
-         * @throws NullPointerException if the key wasn't set
          */
         public MCLeaksAPI build() {
             return new MCLeaksAPIImpl(threadCount, expireAfter, unit);
