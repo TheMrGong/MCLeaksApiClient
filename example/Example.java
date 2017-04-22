fdc1bea0145c2b272392e1d658026bf1859fb141
TheMrGong <gongora656@gmail.com>
2017-04-21 22:21:58 -0400

    Added UUID support, sync checks
    Tests have been added aswell

--- example/Example.java
+++ example/Example.java
@@ -1,5 +1,6 @@
 import me.gong.mcleaks.MCLeaksAPI;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class Example {
@@ -9,8 +10,28 @@ public class Example {
                 .threadCount(2)
                 .expireAfter(10, TimeUnit.MINUTES).build();
 
-        api.checkAccount("BwA_BOOMSTICK", isMCLeaks ->
+        final String playerName = "BwA_BOOMSTICK";
+        final UUID playerUUID = UUID.fromString("071a1906-a49c-4eaa-9156-98df14c2c72b"); // ideally gotten from a real player
+
+        // -- async calling
+
+        // no longer recommended
+        api.checkAccount(playerName, isMCLeaks ->
                 System.out.println("Got: " + isMCLeaks), Throwable::printStackTrace);
+
+        // recommended
+        api.checkAccount(playerUUID, isMCLeaks ->
+                System.out.println("Got: " + isMCLeaks), Throwable::printStackTrace);
+
+        // -- sync calling
+
+        // not recommended
+        final MCLeaksAPI.Result nameResult = api.checkAccount(playerName);
+        // use your result
+
+        // recommended
+        final MCLeaksAPI.Result uuidResult = api.checkAccount(playerUUID);
+        // use your result
 
         // clean up (usually in onDisable)
         api.shutdown();
---
+++ src/test/java/me/gong/junit/APIIntegrationTest.java
@@ -0,0 +1,69 @@
+package me.gong.junit;
+
+import me.gong.mcleaks.MCLeaksAPI;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+@SuppressWarnings("unchecked")
+public class APIIntegrationTest {
+
+    private static final String KNOWN_MCLEAK_NAME = "BwA_BOOMSTICK";
+    private static final UUID KNOWN_MCLEAK_UUID = UUID.fromString("071a1906-a49c-4eaa-9156-98df14c2c72b");
+    private static final long MAX_CACHE_LOOKUP = TimeUnit.SECONDS.toMillis(5);
+
+    private MCLeaksAPI api;
+
+    @Before
+    public void setupApi() {
+        this.api = MCLeaksAPI.builder()
+                .threadCount(2)
+                .expireAfter(10, TimeUnit.MINUTES)
+                .debug()
+                .build();
+    }
+
+    @After
+    public void cleanup() {
+        this.api.shutdown();
+    }
+
+    @Test
+    public void shouldThrowErrorInvalidName() {
+
+        MCLeaksAPI.Result result = api.checkAccount(KNOWN_MCLEAK_NAME + "sefesfsesesefesfsefsefsef");
+        assertTrue("No error was thrown for an invalid name", result.hasError());
+    }
+
+    @Test
+    public void shouldBeMCLeaks() {
+        handleShouldBeMCLeaks(api.checkAccount(KNOWN_MCLEAK_NAME), "name");
+        handleShouldBeMCLeaks(api.checkAccount(KNOWN_MCLEAK_UUID), "uuid");
+    }
+
+    private void handleShouldBeMCLeaks(MCLeaksAPI.Result result, String checkType) {
+        assertFalse(String.format("Error was thrown for valid %s : ", checkType) + result.getError(), result.hasError());
+        assertTrue("Known MCLeaks account wasn't MCLeaks", result.isMCLeaks());
+    }
+
+    @Test
+    public void resultShouldBeCached() {
+        handleResultShouldBeCached(() -> api.checkAccount(KNOWN_MCLEAK_NAME));
+        handleResultShouldBeCached(() -> api.checkAccount(KNOWN_MCLEAK_UUID));
+    }
+
+    private void handleResultShouldBeCached(Runnable checkRemote) {
+        checkRemote.run(); // ignore result
+        final long init = System.currentTimeMillis();
+        checkRemote.run(); // check delay
+
+        // if there's a better way to check if a result is cache'd, let me know at contact@mail.themrgong.xyz
+        assertTrue("Previous result wasn't cache'd", System.currentTimeMillis() - init <= MAX_CACHE_LOOKUP);
+    }
+}
